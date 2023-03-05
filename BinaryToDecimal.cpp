e65a111648cafba8eff87a8735790eb4fdfe4d61
Harshit Raj <harshit.raj2021@vitbhopal.ac.in>
2023-03-05 22:49:50 +0530

    Update BinaryToDecimal.cpp

--- BinaryToDecimal.cpp
+++ BinaryToDecimal.cpp
@@ -1,7 +1,7 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-int binaryToDecimal(int binary){
+int binaryToDecimal(int binary){    //Efficient O(logn)
     int i=0;
     int num=0;
     while(binary!=0){
@@ -14,25 +14,28 @@ int binaryToDecimal(int binary){
     return num;
 }
 
+int binaryToDecimalString(string n){    //O(n) loops n times as length of string
+    int num=0;
+    int j=0;
+    for(int i=n.length()-1;i>=0;i--){
+        if(n[i]=='1') num=pow(2,j)+num;
+        j++;
+    }
+    return num;
+    }
+
 int main(){
     int n=110;
+    string s="110";
     int ans=binaryToDecimal(n);
+    int ans2=binaryToDecimalString(s);
     cout<<ans<<endl;  
+    cout<<ans2<<endl;  
 }
 
 
 
 /*
-MAGIC of SHIFT OPERATORS
->>1 Right shift operator divides by 2
-<<1 left shift operator multiplies by 2
-eg n1=2; b1=10
-
-n2=n1<<1;
-
-n2=4; b2=100
-In n2 bits are shifted to left for once
-
 Algorithm
 Find the last digit of binary using %10;
 if last digit is 1, 1*2^i+ans where i is palce and keep on adding to ans
